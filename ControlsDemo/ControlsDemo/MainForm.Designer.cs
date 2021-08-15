8ad500bd41a2a5e5886deee81cb26ba1172ce86b
luizchequini <luizchequini@gmail.com>
2021-08-15 01:10:48 -0300

    Using combobox control to select an option

--- ControlsDemo/ControlsDemo/MainForm.Designer.cs
+++ ControlsDemo/ControlsDemo/MainForm.Designer.cs
@@ -30,26 +30,26 @@ namespace ControlsDemo
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.BtnClearAll = new System.Windows.Forms.Button();
+            this.BtnDisplay = new System.Windows.Forms.Button();
+            this.CbbOptions = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.cbOptions = new System.Windows.Forms.ComboBox();
-            this.btnDisplay = new System.Windows.Forms.Button();
-            this.btnClearAll = new System.Windows.Forms.Button();
-            this.lstLayer = new System.Windows.Forms.ListBox();
-            this.lstLinetype = new System.Windows.Forms.ListBox();
-            this.lstTextstyle = new System.Windows.Forms.ListBox();
-            this.lblLayerCount = new System.Windows.Forms.Label();
-            this.lblLinetypeCount = new System.Windows.Forms.Label();
-            this.lblTextstyleCount = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LblTextstyleCount = new System.Windows.Forms.Label();
+            this.LblLinetypeCount = new System.Windows.Forms.Label();
+            this.LblLayerCount = new System.Windows.Forms.Label();
+            this.LstTextstyle = new System.Windows.Forms.ListBox();
+            this.LstLinetype = new System.Windows.Forms.ListBox();
+            this.LstLayer = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnClearAll);
-            this.groupBox1.Controls.Add(this.btnDisplay);
-            this.groupBox1.Controls.Add(this.cbOptions);
+            this.groupBox1.Controls.Add(this.BtnClearAll);
+            this.groupBox1.Controls.Add(this.BtnDisplay);
+            this.groupBox1.Controls.Add(this.CbbOptions);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
@@ -58,20 +58,37 @@ namespace ControlsDemo
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Options Container";
             // 
-            // groupBox2
+            // BtnClearAll
             // 
-            this.groupBox2.Controls.Add(this.lblTextstyleCount);
-            this.groupBox2.Controls.Add(this.lblLinetypeCount);
-            this.groupBox2.Controls.Add(this.lblLayerCount);
-            this.groupBox2.Controls.Add(this.lstTextstyle);
-            this.groupBox2.Controls.Add(this.lstLinetype);
-            this.groupBox2.Controls.Add(this.lstLayer);
-            this.groupBox2.Location = new System.Drawing.Point(305, 12);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(513, 426);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Display Container";
+            this.BtnClearAll.Location = new System.Drawing.Point(123, 397);
+            this.BtnClearAll.Name = "BtnClearAll";
+            this.BtnClearAll.Size = new System.Drawing.Size(111, 23);
+            this.BtnClearAll.TabIndex = 3;
+            this.BtnClearAll.Text = "Clear All";
+            this.BtnClearAll.UseVisualStyleBackColor = true;
+            // 
+            // BtnDisplay
+            // 
+            this.BtnDisplay.Location = new System.Drawing.Point(6, 397);
+            this.BtnDisplay.Name = "BtnDisplay";
+            this.BtnDisplay.Size = new System.Drawing.Size(111, 23);
+            this.BtnDisplay.TabIndex = 2;
+            this.BtnDisplay.Text = "Show Items";
+            this.BtnDisplay.UseVisualStyleBackColor = true;
+            this.BtnDisplay.Click += new System.EventHandler(this.BtnDisplay_Click);
+            // 
+            // CbbOptions
+            // 
+            this.CbbOptions.FormattingEnabled = true;
+            this.CbbOptions.Items.AddRange(new object[] {
+            "All",
+            "Layer",
+            "Linetype",
+            "Textstyle"});
+            this.CbbOptions.Location = new System.Drawing.Point(6, 37);
+            this.CbbOptions.Name = "CbbOptions";
+            this.CbbOptions.Size = new System.Drawing.Size(275, 24);
+            this.CbbOptions.TabIndex = 1;
             // 
             // label1
             // 
@@ -82,90 +99,74 @@ namespace ControlsDemo
             this.label1.TabIndex = 0;
             this.label1.Text = "Select item to display:";
             // 
-            // cbOptions
+            // groupBox2
             // 
-            this.cbOptions.FormattingEnabled = true;
-            this.cbOptions.Items.AddRange(new object[] {
-            "All",
-            "Layer",
-            "Linetype",
-            "Textstyle"});
-            this.cbOptions.Location = new System.Drawing.Point(6, 37);
-            this.cbOptions.Name = "cbOptions";
-            this.cbOptions.Size = new System.Drawing.Size(275, 24);
-            this.cbOptions.TabIndex = 1;
+            this.groupBox2.Controls.Add(this.LblTextstyleCount);
+            this.groupBox2.Controls.Add(this.LblLinetypeCount);
+            this.groupBox2.Controls.Add(this.LblLayerCount);
+            this.groupBox2.Controls.Add(this.LstTextstyle);
+            this.groupBox2.Controls.Add(this.LstLinetype);
+            this.groupBox2.Controls.Add(this.LstLayer);
+            this.groupBox2.Location = new System.Drawing.Point(305, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(513, 426);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Display Container";
             // 
-            // btnDisplay
+            // LblTextstyleCount
             // 
-            this.btnDisplay.Location = new System.Drawing.Point(6, 397);
-            this.btnDisplay.Name = "btnDisplay";
-            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
-            this.btnDisplay.TabIndex = 2;
-            this.btnDisplay.Text = "Show Items";
-            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.LblTextstyleCount.AutoSize = true;
+            this.LblTextstyleCount.Location = new System.Drawing.Point(341, 400);
+            this.LblTextstyleCount.Name = "LblTextstyleCount";
+            this.LblTextstyleCount.Size = new System.Drawing.Size(17, 16);
+            this.LblTextstyleCount.TabIndex = 5;
+            this.LblTextstyleCount.Text = "...";
             // 
-            // btnClearAll
+            // LblLinetypeCount
             // 
-            this.btnClearAll.Location = new System.Drawing.Point(87, 397);
-            this.btnClearAll.Name = "btnClearAll";
-            this.btnClearAll.Size = new System.Drawing.Size(75, 23);
-            this.btnClearAll.TabIndex = 3;
-            this.btnClearAll.Text = "Clear All";
-            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.LblLinetypeCount.AutoSize = true;
+            this.LblLinetypeCount.Location = new System.Drawing.Point(172, 400);
+            this.LblLinetypeCount.Name = "LblLinetypeCount";
+            this.LblLinetypeCount.Size = new System.Drawing.Size(17, 16);
+            this.LblLinetypeCount.TabIndex = 4;
+            this.LblLinetypeCount.Text = "...";
             // 
-            // lstLayer
+            // LblLayerCount
             // 
-            this.lstLayer.FormattingEnabled = true;
-            this.lstLayer.ItemHeight = 16;
-            this.lstLayer.Location = new System.Drawing.Point(6, 21);
-            this.lstLayer.Name = "lstLayer";
-            this.lstLayer.Size = new System.Drawing.Size(163, 372);
-            this.lstLayer.TabIndex = 0;
+            this.LblLayerCount.AutoSize = true;
+            this.LblLayerCount.Location = new System.Drawing.Point(6, 400);
+            this.LblLayerCount.Name = "LblLayerCount";
+            this.LblLayerCount.Size = new System.Drawing.Size(17, 16);
+            this.LblLayerCount.TabIndex = 3;
+            this.LblLayerCount.Text = "...";
             // 
-            // lstLinetype
+            // LstTextstyle
             // 
-            this.lstLinetype.FormattingEnabled = true;
-            this.lstLinetype.ItemHeight = 16;
-            this.lstLinetype.Location = new System.Drawing.Point(175, 21);
-            this.lstLinetype.Name = "lstLinetype";
-            this.lstLinetype.Size = new System.Drawing.Size(163, 372);
-            this.lstLinetype.TabIndex = 1;
+            this.LstTextstyle.FormattingEnabled = true;
+            this.LstTextstyle.ItemHeight = 16;
+            this.LstTextstyle.Location = new System.Drawing.Point(344, 21);
+            this.LstTextstyle.Name = "LstTextstyle";
+            this.LstTextstyle.Size = new System.Drawing.Size(163, 372);
+            this.LstTextstyle.TabIndex = 2;
             // 
-            // lstTextstyle
+            // LstLinetype
             // 
-            this.lstTextstyle.FormattingEnabled = true;
-            this.lstTextstyle.ItemHeight = 16;
-            this.lstTextstyle.Location = new System.Drawing.Point(344, 21);
-            this.lstTextstyle.Name = "lstTextstyle";
-            this.lstTextstyle.Size = new System.Drawing.Size(163, 372);
-            this.lstTextstyle.TabIndex = 2;
+            this.LstLinetype.FormattingEnabled = true;
+            this.LstLinetype.ItemHeight = 16;
+            this.LstLinetype.Location = new System.Drawing.Point(175, 21);
+            this.LstLinetype.Name = "LstLinetype";
+            this.LstLinetype.Size = new System.Drawing.Size(163, 372);
+            this.LstLinetype.TabIndex = 1;
             // 
-            // lblLayerCount
+            // LstLayer
             // 
-            this.lblLayerCount.AutoSize = true;
-            this.lblLayerCount.Location = new System.Drawing.Point(6, 400);
-            this.lblLayerCount.Name = "lblLayerCount";
-            this.lblLayerCount.Size = new System.Drawing.Size(17, 16);
-            this.lblLayerCount.TabIndex = 3;
-            this.lblLayerCount.Text = "...";
-            // 
-            // lblLinetypeCount
-            // 
-            this.lblLinetypeCount.AutoSize = true;
-            this.lblLinetypeCount.Location = new System.Drawing.Point(172, 400);
-            this.lblLinetypeCount.Name = "lblLinetypeCount";
-            this.lblLinetypeCount.Size = new System.Drawing.Size(17, 16);
-            this.lblLinetypeCount.TabIndex = 4;
-            this.lblLinetypeCount.Text = "...";
-            // 
-            // lblTextstyleCount
-            // 
-            this.lblTextstyleCount.AutoSize = true;
-            this.lblTextstyleCount.Location = new System.Drawing.Point(341, 400);
-            this.lblTextstyleCount.Name = "lblTextstyleCount";
-            this.lblTextstyleCount.Size = new System.Drawing.Size(17, 16);
-            this.lblTextstyleCount.TabIndex = 5;
-            this.lblTextstyleCount.Text = "...";
+            this.LstLayer.FormattingEnabled = true;
+            this.LstLayer.ItemHeight = 16;
+            this.LstLayer.Location = new System.Drawing.Point(6, 21);
+            this.LstLayer.Name = "LstLayer";
+            this.LstLayer.Size = new System.Drawing.Size(163, 372);
+            this.LstLayer.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -190,15 +191,15 @@ namespace ControlsDemo
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox cbOptions;
+        private System.Windows.Forms.ComboBox CbbOptions;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnDisplay;
-        private System.Windows.Forms.Button btnClearAll;
-        private System.Windows.Forms.ListBox lstTextstyle;
-        private System.Windows.Forms.ListBox lstLinetype;
-        private System.Windows.Forms.ListBox lstLayer;
-        private System.Windows.Forms.Label lblLinetypeCount;
-        private System.Windows.Forms.Label lblLayerCount;
-        private System.Windows.Forms.Label lblTextstyleCount;
+        private System.Windows.Forms.Button BtnDisplay;
+        private System.Windows.Forms.Button BtnClearAll;
+        private System.Windows.Forms.ListBox LstTextstyle;
+        private System.Windows.Forms.ListBox LstLinetype;
+        private System.Windows.Forms.ListBox LstLayer;
+        private System.Windows.Forms.Label LblLinetypeCount;
+        private System.Windows.Forms.Label LblLayerCount;
+        private System.Windows.Forms.Label LblTextstyleCount;
     }
 }
--- ControlsDemo/ControlsDemo/MainForm.cs
+++ ControlsDemo/ControlsDemo/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,51 @@ namespace ControlsDemo
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            string choice = "";
+
+            choice = CbbOptions.SelectedItem.ToString();
+            ControlsDemoUtil controlsDemoUtil = new ControlsDemoUtil();
+
+            if (choice == "Layer")
+            {
+                ArrayList layerList = controlsDemoUtil.GetLayers();
+                LstLayer.DataSource = layerList;
+                LstLinetype.DataSource = null;
+                LstTextstyle.DataSource = null;
+                LblLayerCount.Text = "Layers count: " + layerList.Count;
+            }
+            else if (choice == "Linetype")
+            {
+                ArrayList linetypeList = controlsDemoUtil.GetLinetypes();
+                LstLayer.DataSource = null;
+                LstLinetype.DataSource = linetypeList;
+                LstTextstyle.DataSource = null;
+                LblLinetypeCount.Text = "Linetype count: " + linetypeList.Count;
+            }
+            else if (choice == "Textstyle")
+            {
+                ArrayList textstyleList = controlsDemoUtil.GetTextstyles();
+                LstLayer.DataSource = null;
+                LstLinetype.DataSource = null;
+                LstTextstyle.DataSource = textstyleList;
+                LblTextstyleCount.Text = "Textstyle count: " + textstyleList.Count;
+            }
+            else if (choice == "All")
+            {
+                ArrayList layerList = controlsDemoUtil.GetLayers();
+                ArrayList linetypeList = controlsDemoUtil.GetLinetypes();
+                ArrayList textstyleList = controlsDemoUtil.GetTextstyles();
+                LstLayer.DataSource = layerList;
+                LstLinetype.DataSource = linetypeList;
+                LstTextstyle.DataSource = textstyleList;
+                LblLayerCount.Text = "Layers count: " + layerList.Count;
+                LblLinetypeCount.Text = "Linetype count: " + linetypeList.Count;
+                LblTextstyleCount.Text = "Textstyle count: " + textstyleList.Count;
+            }
         }
     }
 }
